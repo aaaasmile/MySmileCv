f05457fcd09efa26832bc4ea8e8a6b2b414c4425
aaaasmile <testhaus@gmx.at>
2016-03-08 13:19:19 +0100

    use bundle

--- rails/config/boot.rb
+++ rails/config/boot.rb
@@ -41,5 +41,11 @@ unless defined?(Rails::Initializer)
     end
   end
 
+  Rails::Initializer.class_eval do
+    def load_gems
+		@bundler_loaded ||= Bundler.require :default, Rails.env
+    end
+  end
+
   Rails::Initializer.run(:set_load_path)
 end
