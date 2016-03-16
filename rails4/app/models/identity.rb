34388742653a87c541f037796fe3ac3bec867786
aaaasmile <testhaus@gmx.at>
2016-03-16 16:12:58 +0100

    fix fax

--- rails4/app/models/identity.rb
+++ rails4/app/models/identity.rb
@@ -2,7 +2,7 @@ class Identity < ActiveRecord::Base
   belongs_to(:language, {:foreign_key => 'klang'})
   
   def info_compact
-    "#{firstname}, #{lastname}, #{address}, #{mobile}, #{email}, #{web}, #{birthdate}, #{gender}, #{nationality}, #{fax}, #{familystate}"
+    "#{firstname}, #{lastname}, #{address}, #{mobile}, #{email}, #{web}, #{birthdate}, #{gender}, #{nationality}, #{other}, #{familystate}"
   end
   
 end
